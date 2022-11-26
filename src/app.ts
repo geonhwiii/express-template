5bf02079b0158db0a0469632403a11bd5687de58
geonhwiii <geonhwiii@github.com>
2022-11-26 16:33:39 +0900

    add middleware

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,20 @@ const port: number = 8000;
 
 app.get('/', (req: express.Request, res: express.Response) => {
 	res.send({
+		result: { message: 'success' },
+		error: null,
+	});
+});
+
+app.get('/cats', (req, res) => {
+	res.send({
 		result: { cats: Cat },
 		error: null,
 	});
+});
+
+app.use((req, res) => {
+	res.send({ error: '404 NOT FOUND' });
 });
 
 app.listen(port, () => {
