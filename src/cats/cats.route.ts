fcc85bd8787641f4f1756ac1147ecfa7df54d2a3
geonhwiii <geonhwiii@github.com>
2022-11-26 17:18:08 +0900

    add rud

--- src/cats/cats.route.ts
+++ src/cats/cats.route.ts
@@ -1,46 +1,129 @@
+import { Cat, CatType } from './cats.model';
 import { Router } from 'express';
-import { Cat } from './cats.model';
 
 const router = Router();
 
-/* [READ]: Get All Cats */
+//* [READ]
 router.get('/cats', (req, res) => {
 	try {
 		const cats = Cat;
 		res.status(200).send({
-			result: { data: cats, success: true },
-			error: null,
+			success: true,
+			data: {
+				cats,
+			},
 		});
 	} catch (error: any) {
-		res.status(400).send({ success: false, message: error.message, error: 'SERVER ERROR' });
+		res.status(400).send({
+			success: false,
+			error: error.message,
+		});
 	}
 });
 
-/* [READ]: Get one cat */
+//* [READ]
 router.get('/cats/:id', (req, res) => {
 	try {
-		const id = req.params.id;
-		const cat = Cat.find((item) => item.id === id);
+		const params = req.params;
+		console.log(params);
+		const cat = Cat.find((cat) => {
+			return cat.id === params.id;
+		});
 		res.status(200).send({
-			result: { data: cat, success: true },
-			error: null,
+			success: true,
+			data: {
+				cat,
+			},
 		});
 	} catch (error: any) {
-		res.status(400).send({ success: false, message: error.message, error: 'SERVER ERROR' });
+		res.status(400).send({
+			success: false,
+			error: error.message,
+		});
 	}
 });
 
-/* [CREATE]: Create a cat */
+//* CREATE 새로운 고양이 추가 api -> POST
 router.post('/cats', (req, res) => {
 	try {
-		const body = req.body;
-		Cat.push(body);
+		const data = req.body;
+		Cat.push(data); // create
 		res.status(200).send({
-			result: { data: body, success: true },
-			error: null,
+			success: true,
+			data: { data },
 		});
 	} catch (error: any) {
-		res.status(400).send({ success: false, message: error.message, error: 'SERVER ERROR' });
+		res.status(400).send({
+			success: false,
+			error: error.message,
+		});
+	}
+});
+
+//* [UPDATE]
+router.put('/cats/:id', (req, res) => {
+	try {
+		const params = req.params;
+		const body = req.body;
+		let result = Cat.map((cat) => {
+			if (cat.id === params.id) {
+				return body;
+			}
+			return cat;
+		});
+		res.status(200).send({
+			success: true,
+			data: {
+				cat: result,
+			},
+		});
+	} catch (error: any) {
+		res.status(400).send({
+			success: false,
+			error: error.message,
+		});
+	}
+});
+
+//* [UPDATE]
+router.patch('/cats/:id', (req, res) => {
+	try {
+		const params = req.params;
+		const body = req.body;
+		let result = Cat.map((cat) => {
+			if (cat.id === params.id) {
+				cat = { ...cat, ...body };
+			}
+			return cat;
+		});
+		res.status(200).send({
+			success: true,
+			data: {
+				cat: result,
+			},
+		});
+	} catch (error: any) {
+		res.status(400).send({
+			success: false,
+			error: error.message,
+		});
+	}
+});
+
+//* [DELETE]
+router.delete('/cats/:id', (req, res) => {
+	try {
+		const params = req.params;
+		const newCat = Cat.filter((cat) => cat.id !== params.id);
+		res.status(200).send({
+			success: true,
+			data: newCat,
+		});
+	} catch (error: any) {
+		res.status(400).send({
+			success: false,
+			error: error.message,
+		});
 	}
 });
 
